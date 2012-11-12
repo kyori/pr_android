b6a314dcf4b83fd8f8760603ffd1a2f7466d6cf0
kyori <happykyori@gmail.com>
2012-11-12 21:54:07 +0900

    filterselection add

--- gen/com/example/firstandroid/R.java
+++ gen/com/example/firstandroid/R.java
@@ -15,32 +15,36 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int allDay=0x7f07000d;
+        public static final int allDay=0x7f070010;
         public static final int backBtn=0x7f070005;
-        public static final int btnExit=0x7f07000b;
+        public static final int btnExit=0x7f07000e;
         public static final int cameraBtn=0x7f070003;
-        public static final int downloadBtn=0x7f07000f;
+        public static final int downloadBtn=0x7f070012;
         public static final int f_title=0x7f070000;
+        public static final int filter01=0x7f070007;
+        public static final int filter02=0x7f070008;
+        public static final int filter03=0x7f070009;
         public static final int galleryBtn=0x7f070004;
-        public static final int menu_settings=0x7f070010;
+        public static final int menu_settings=0x7f070013;
         public static final int nextBtn=0x7f070006;
         public static final int radio01=0x7f070002;
-        public static final int radio02=0x7f07000c;
+        public static final int radio02=0x7f07000f;
         public static final int radioGroup01=0x7f070001;
-        public static final int shareBtn=0x7f07000e;
-        public static final int start02Btn=0x7f070008;
-        public static final int start03Btn=0x7f070009;
-        public static final int start04Btn=0x7f07000a;
-        public static final int textView1=0x7f070007;
+        public static final int shareBtn=0x7f070011;
+        public static final int start02Btn=0x7f07000b;
+        public static final int start03Btn=0x7f07000c;
+        public static final int start04Btn=0x7f07000d;
+        public static final int textView1=0x7f07000a;
     }
     public static final class layout {
         public static final int d_layout=0x7f030000;
         public static final int f_layout=0x7f030001;
-        public static final int layout1=0x7f030002;
-        public static final int layout3=0x7f030003;
-        public static final int m_layout=0x7f030004;
-        public static final int new_layout=0x7f030005;
-        public static final int result_layout=0x7f030006;
+        public static final int fs_layout=0x7f030002;
+        public static final int layout1=0x7f030003;
+        public static final int layout3=0x7f030004;
+        public static final int m_layout=0x7f030005;
+        public static final int new_layout=0x7f030006;
+        public static final int result_layout=0x7f030007;
     }
     public static final class menu {
         public static final int layout1=0x7f060000;
--- src/com/example/firstandroid/Filterselection.java
+++ src/com/example/firstandroid/Filterselection.java
@@ -12,31 +12,31 @@ import android.view.View.*;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Filter extends Activity {
+public class Filterselection extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.f_layout);
+        setContentView(R.layout.fs_layout);
 
-        Button cameraBtn = (Button)findViewById(R.id.cameraBtn);
-        cameraBtn.setOnClickListener(new OnClickListener() {
-     	   public void onClick(View v) {
- 			Toast.makeText(getApplicationContext(), "카메라 버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
- 		//	finish();
- 		}
-     	   
-        });
+//        Button cameraBtn = (Button)findViewById(R.id.cameraBtn);
+//        cameraBtn.setOnClickListener(new OnClickListener() {
+//     	   public void onClick(View v) {
+// 			Toast.makeText(getApplicationContext(), "카메라 버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
+// 		//	finish();
+// 		}
+//     	   
+//        });
         
-        
-       Button galleryBtn = (Button)findViewById(R.id.galleryBtn);
-       galleryBtn.setOnClickListener(new OnClickListener() {
-    	   public void onClick(View v) {
-			Toast.makeText(getApplicationContext(), "갤러리 버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
-		//	finish();
-		}
-    	   
-       });
+//        
+//       Button galleryBtn = (Button)findViewById(R.id.galleryBtn);
+//       galleryBtn.setOnClickListener(new OnClickListener() {
+//    	   public void onClick(View v) {
+//			Toast.makeText(getApplicationContext(), "갤러리 버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
+//		//	finish();
+//		}
+//    	   
+//       });
 
        Button backBtn = (Button)findViewById(R.id.backBtn);
        backBtn.setOnClickListener(new OnClickListener() {
@@ -51,7 +51,7 @@ public class Filter extends Activity {
        Button nextBtn = (Button)findViewById(R.id.nextBtn);
        nextBtn.setOnClickListener(new OnClickListener() {
     	   public void onClick(View v) {
-        	    Intent myIntent = new Intent(getApplicationContext(), Filterselection.class);
+        	    Intent myIntent = new Intent(getApplicationContext(), Result.class);
         	    startActivity(myIntent);
         	    finish();
    		}
