b15c0ecd000dc5f05fd0a3a7e5446828cf03cf8e
kyori <happykyori@gmail.com>
2012-11-18 18:34:02 +0900

    add imageview

--- gen/com/example/firstandroid/R.java
+++ gen/com/example/firstandroid/R.java
@@ -11,26 +11,30 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int android=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int sample=0x7f020003;
     }
     public static final class id {
-        public static final int allDay=0x7f070010;
-        public static final int backBtn=0x7f070005;
+        public static final int allDay=0x7f070011;
+        public static final int backBtn=0x7f070004;
         public static final int btnExit=0x7f07000e;
-        public static final int cameraBtn=0x7f070003;
-        public static final int downloadBtn=0x7f070012;
+        public static final int cameraBtn=0x7f070002;
+        public static final int downloadBtn=0x7f070014;
         public static final int f_title=0x7f070000;
         public static final int filter01=0x7f070007;
         public static final int filter02=0x7f070008;
         public static final int filter03=0x7f070009;
-        public static final int galleryBtn=0x7f070004;
-        public static final int menu_settings=0x7f070013;
-        public static final int nextBtn=0x7f070006;
-        public static final int radio01=0x7f070002;
-        public static final int radio02=0x7f07000f;
-        public static final int radioGroup01=0x7f070001;
-        public static final int shareBtn=0x7f070011;
+        public static final int galleryBtn=0x7f070003;
+        public static final int menu_settings=0x7f070015;
+        public static final int nextBtn=0x7f070005;
+        public static final int origin=0x7f070001;
+        public static final int origin2=0x7f070012;
+        public static final int radio01=0x7f07000f;
+        public static final int radio02=0x7f070010;
+        public static final int radioGroup01=0x7f070006;
+        public static final int shareBtn=0x7f070013;
         public static final int start02Btn=0x7f07000b;
         public static final int start03Btn=0x7f07000c;
         public static final int start04Btn=0x7f07000d;
